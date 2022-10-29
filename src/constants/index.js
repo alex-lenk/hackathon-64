81cab3e9c26f405232e33c2b5ce238a9edbfe9c6
alex-lenk <alexlenk.ru@gmail.com>
2022-10-29 10:47:15 +0300

    Created core for team development

---
+++ src/constants/index.js
@@ -0,0 +1,2 @@
+export const body = document.body
+export const classOpen = 'open'
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,69 @@
 import {Menu} from './core/menu'
+import {classOpen} from './constants'
 
-export class ContextMenu extends Menu {
+export default class ContextMenu extends Menu {
+  constructor(selector, settings) {
+    super(selector)
+    this.modules = settings.modules
+  }
 
+  open() {
+    this.el.classList.add(classOpen)
+  }
+
+  close() {
+    this.el.classList.remove(classOpen)
+  }
+
+  add(module) {
+    this.el.innerHTML = module
+  }
+
+  purge() {
+    const filteredElements = [].slice.call(this.el.parentNode.children).filter(child => {
+      return child !== this.el
+    })
+    filteredElements.forEach(el => el.remove())
+  }
+
+  renderMenu() {
+    this.modules = this.modules.map(Module => new Module)
+
+    this.modules.forEach(instance => {
+      const moduleElement = instance.toHTML()
+
+      this.add(moduleElement)
+    })
+  }
+
+  run() {
+    this.renderMenu()
+
+    this.el.addEventListener('click', ({target}) => {
+      if (!target.classList.contains('menu-item')) return false
+
+      this.purge()
+      this.close()
+
+      const clickedModuleInstance = this.modules.filter(moduleInstance => {
+        return moduleInstance.type === target.dataset.type
+      })[0]
+
+      clickedModuleInstance.trigger()
+    })
+
+    document.addEventListener('contextmenu', (event) => {
+      event.preventDefault()
+
+      const {pageX, pageY} = event
+      this.el.style.left = `${pageX}px`
+      this.el.style.top = `${pageY}px`
+
+      if (this.modules !== [] && this.el.classList.contains(classOpen)) {
+        return false
+      } else {
+        this.open()
+      }
+    })
+  }
 }
