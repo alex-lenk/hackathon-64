b493c9ba3e51c30ecb7edb10443df785ec054e82
Maksim Senin <wcpzor@gmail.com>
2022-10-29 13:34:34 +0300

    Implemented background module

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import './assets/styles.css'
 import ContextMenu from './menu'
 import {ShapeModule} from './modules/shape.module'
+import {BackgroundModule} from './modules/background.module'
 
 const menu = new ContextMenu('#menu', {
   modules: [
     ShapeModule,
+    BackgroundModule,
   ]
 })
 
