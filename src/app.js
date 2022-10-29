5a9baa9785a08427ecc9eea215ec6c439ead3e99
Rustam Muratov <rustmalkovich55@gmail.com>
2022-10-29 21:09:34 +0300

    created messege module

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import './assets/styles.css'
 import ContextMenu from './menu'
 import {ShapeModule} from './modules/shape.module'
+import {MessegeModule} from './modules/messege.module'
 
 const menu = new ContextMenu('#menu', {
   modules: [
     ShapeModule,
+    MessegeModule
   ]
 })
 
