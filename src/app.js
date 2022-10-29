a4718db158ae142f92cfc13cb19bd6f73b6213dc
alex-lenk <alexlenk.ru@gmail.com>
2022-10-29 10:59:07 +0300

    Created shape module

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import './assets/styles.css'
 import ContextMenu from './menu'
+import {ShapeModule} from './modules/shape.module'
 
 const menu = new ContextMenu('#menu', {
   modules: [
+    ShapeModule,
   ]
 })
 
