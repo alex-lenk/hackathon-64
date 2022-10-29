e20b6cbec25fd6ae1e32a6e1b05a020e61ce14b8
Rustam Muratov <116566231+RustMalkovich@users.noreply.github.com>
2022-10-29 21:35:10 +0300

    Created message module

--- src/app.js
+++ src/app.js
@@ -2,11 +2,13 @@ import './assets/styles.css'
 import ContextMenu from './menu'
 import ShapeModule from './modules/shape.module'
 import {BackgroundModule} from './modules/background.module'
+import {MessegeModule} from './modules/messege.module'
 
 const menu = new ContextMenu('#menu', {
   modules: [
     ShapeModule,
     BackgroundModule,
+    MessegeModule
   ]
 })
 
