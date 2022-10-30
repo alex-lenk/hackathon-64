cf41ad5254f68a55cb078ae74e29922f6cc561a2
Maksim Senin <wcpzor@gmail.com>
2022-10-30 11:22:44 +0300

    Refactored background-module

--- src/modules/background.module.js
+++ src/modules/background.module.js
@@ -3,25 +3,22 @@ import random from '../utils/random'
 import {body} from '../constants'
 
 export class BackgroundModule extends Module {
-    #colors
-
-  static TYPE = 'BackgroundModule'
-  static TEXT = 'Поменять цвет'
+  #colors
 
   constructor() {
-    super(BackgroundModule.TYPE, BackgroundModule.TEXT)
+    super('BackgroundModule', 'Поменять цвет')
     this.#colors = [
       '#4652FF', '#616FFF', '#A7B8FF', '#1D00B0', '#342EDD', '#ECF0FF', '#FC612E', '#FF37BB', '#8CCA08', '#2FD1BE',
       '#57D08E', '#956CED', '#FFCA2A', '#282828', '#7C7B7B', '#747474',
     ]
   }
 
-  getRandomColor = () => {
+  #getRandomColor = () => {
     const index = random(0, this.#colors.length - 1)
     return this.#colors[index]
   }
 
   trigger() {
-    body.style.background = this.getRandomColor()
+    body.style.background = this.#getRandomColor()
   }
 }
